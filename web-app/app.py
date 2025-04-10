514e18a7294a69fc57ee8b60209ea9227e02e15f
JeffreyChen112 <jeffreychen112@gmail.com>
2025-04-10 18:32:14 -0400

    minor edits

--- web-app/app.py
+++ web-app/app.py
@@ -325,5 +325,5 @@ def create_app():
 
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=5002)
+    flaskapp = create_app()
+    flaskapp.run(host="0.0.0.0", port=5002)
